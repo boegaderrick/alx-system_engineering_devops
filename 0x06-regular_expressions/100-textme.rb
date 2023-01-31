3ce8a955dd5eb00dfd1fc214f9e8575fb90b3733
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-31 20:56:16 +0300

    regular expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
-File.open("text_messages.log", "r").each_line do |log|
-  from = log.scan(/from:([^\]]+)/).join
-  to = log.scan(/to:([^\]]+)/).join
-  flags = log.scan(/flags:([^\]]+)/).join
-  puts from + "," + to + "," + flags + "\n"
-end
+#File.open("text_messages.log", "r").each_line do |log|
+#  from = log.scan(/from:([^\]]+)/).join
+#  to = log.scan(/to:([^\]]+)/).join
+#  flags = log.scan(/flags:([^\]]+)/).join
+#  puts from + "," + to + "," + flags + "\n"
+#end
+
+puts ARGV[0].scan(/^.+from:([^\]]+)|to:([^\]]+)|flags:([^\]]+)/).flatten.compact.join(",")
