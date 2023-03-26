8d6e94860575bf6b2d06cf632e1e5a1aa690b333
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-27 02:46:12 +0300

    puppet ssh_config mod

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,12 @@
-#This script modifies the ssh_config
-file {'~/.ssh/config':
-	PasswordAuthentication => no,
-	IdentityFile => ~/.ssh/school,
-	}
+#This script modifies ssh_config
+_file { 'Password':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+_file { 'Identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
