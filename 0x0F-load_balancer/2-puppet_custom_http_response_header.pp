77c86e90f17450c0bbe09d7857223481b0c4194f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-04 02:48:24 +0300

    puppet custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,15 @@
 # This manifest mods a server conf file to include custom header
 
 package {'nginx':
-	ensure => present,
-} ->
-file {'/etc/nginx/conf.d/cus_headers.conf':
-	owner => 'root',
-	group => 'root',
-	mode => '0644',
-	content => 'add_header X-Served-By $hostname;',
-} ->
-service {'nginx':
-	ensure => 'running',
-	require => File['/etc/nginx/conf.d/cus_headers.conf'],
+  ensure => present,
+}
+-> file {'/etc/nginx/conf.d/cus_headers.conf':
+  content => 'add_header X-Served-By $hostname;',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
+-> service {'nginx':
+  ensure  => 'running',
+  require => File['/etc/nginx/conf.d/cus_headers.conf'],
 }
