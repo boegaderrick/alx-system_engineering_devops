ad2f6ed8d73fa25779b1abeeb7dd67e4884df1a7
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-24 17:34:15 +0300

    data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module request an api for workers and all assignments
+    and then prints a worker's name and their progress
+    It takes a single command-line argument which is the ID of
+    the subject worker.
+"""
+from requests import get
+from json import loads
+from sys import argv
+
+users = get('https://jsonplaceholder.typicode.com/users/')
+user = [i for i in loads(users.text) if str(i.get('id')) == argv[1]][0]
+
+data = get('https://jsonplaceholder.typicode.com/todos/')
+tasks = [i for i in loads(data.text) if str(i['userId']) == argv[1]]
+complete = [i for i in tasks if i.get('completed')]
+
+total = len(tasks)
+done = len(complete)
+print(f'Employee {user.get("name")} is done with tasks({done}/{total})')
+for i in complete:
+    print('\t' + i.get('title'))
